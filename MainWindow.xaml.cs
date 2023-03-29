b21185eada2694b60a1572c25d98c4e7c0847d09
crossettben <crossettben@gmail.com>
2023-03-29 09:46:11 -0700

    Button Functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,24 @@ namespace AsyncAwaitDemo
         {
             InitializeComponent();
         }
+
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            //We need to do some fancy calculation the slows down the UI.
+            StatusLabel.Content = "START - Running Syncronous";
+            Thread.Sleep(5000);
+            StatusLabel.Content = "END - Running Syncronous";
+
+        }
+
+
+        private async void AsyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLabel.Content = "START - Running Asyncronous";
+            await Task.Run(() => Thread.Sleep(5000));
+            StatusLabel.Content = "END - Running Asyncronous";
+        }
+
     }
 }
